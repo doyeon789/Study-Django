90ae3e49f74c41cd94dc9f21e12b61296854798c
doyeon789 <ldyeon789@naver.com>
2026-04-07 11:07:13 +0900

    add app and use temp

--- 01. django_intro_design_pattern/live/articles/views.py
+++ 01. django_intro_design_pattern/live/articles/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
-
+import random
 # Create our views here
 # django에서 view함수를 만들때느는 항상
 # 첫번째 인자로 request를 저으이
 #   이 reqest 인자에 사용자의 요청정보가 모두 포함되어 있다.
 #   
 def index(request):
-    # reder(request, template의 경로)
-    # templates/ <- 요거 생략하고 작성
-    # app_name/templates/app_name
-    return render(request, 'articles/index.html')
+    name = 'doyeon'
+    context = {
+        'name' : name
+    }
+    return render(request, 'articles/index.html', context)
+
+def dinner(request):
+    foods = ['국밥', '국수', '카레', '탕수육']
+    picked = random.choice(foods)
+    context = {
+        'foods' : foods,
+        'picked' : picked
+    }
+    return render(request, 'articles/dinner.html', context)
+
+def search(request):
+    return render(request, 'articles/search.html')
+
+def throw(request):
+    return render(request, 'articles/throw.html')
+
+def catch(request):
+    # 이 함수가 throw에서 건네는 데이터를 받는 역할
+
+    message = request.GET.get('message')
+    context = {
+        'message' : message
+    }
+
+    return render(request, 'articles/catch.html', context)
+
+def detail(request, num):
+    context = {
+        'article_id':num
+    }
+    return render(request, 'articles/detail.html',context)
