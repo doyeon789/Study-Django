052d6e7e3bd5c732db30b78902348dd7adacd892
doyeon789 <ldyeon789@naver.com>
2026-05-06 09:28:58 +0900

    feat: m:m 필드 생성

--- 99_mtm_practice/hospitals/models.py
+++ 99_mtm_practice/hospitals/models.py
@@ -8,18 +8,13 @@ class Doctor(models.Model):
         return f'{self.pk}번 의사 {self.name}'
 
 
-# 외래키 삭제
 class Patient(models.Model):
+    # ManyToManyField 작성
+    doctors = models.ManyToManyField(Doctor)
     name = models.TextField()
 
     def __str__(self):
         return f'{self.pk}번 환자 {self.name}'
 
 
-# 중개모델 작성
-class Reservation(models.Model):
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.doctor_id}번 의사의 {self.patient_id}번 환자'
+# Reservation Class 주석 처리
